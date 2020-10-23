d7ab4ae1f884e1ed0e64b4b0d33c893006a17feb
B-e-r-i-ya <60294062+B-e-r-i-ya@users.noreply.github.com>
2020-10-23 20:11:25 +0700

    Update ps.sh

--- ps.sh
+++ ps.sh
@@ -5,14 +5,19 @@ do
 
 	tty=$(cat 2>/dev/null /proc/$pid/stat | awk '{print $7}')
 	stat=$(cat 2>/dev/null /proc/$pid/stat | awk '{print $3}')
-	utime=$(cat 2>/dev/null /proc/$pid/stat | awk '{print $14}')
+	time=$(cat 2>/dev/null /proc/$pid/stat | awk '{print $14}')
 	stime=$(cat  2>/dev/null /proc/$pid/stat | awk '{print $17}')
+	
 	if [ -f "/proc/${pid}/cmdline" ] 
 	then 
-			IFS= read -d '' test</proc/$pid/cmdline||[[ $test ]]
-			cmd=`echo ${test}|awk '{print $1}'`
+			IFS= read -d '' cmd</proc/$pid/cmdline||[[ $cmd ]]
+			cmd=`echo ${cmd}|awk '{print $1}'`
+			
 	fi
-
-	printf "%-8s | %-15s | %s\n" "$pid | $tty | $stat | $time | $cmd" | column -t  -s '|'
+	RED='\033[0;31m'
+	YELLOW='\033[0;33m'
+	BLUE='\033[0;34m'
+	NORM='\033[0m'
+	echo -e "| $pid | $YELLOW $tty $NORM| $RED $stat $NORM| $time | $BLUE $cmd $NORM"|column -t -s '|' -n
 
 done
